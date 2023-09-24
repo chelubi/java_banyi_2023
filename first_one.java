ea3e25c4182f74619fc736a574e8485ad90bf658
chelubi <72791867+chelubi@users.noreply.github.com>
2023-09-24 21:19:46 +0300

    Add files via upload

---
+++ first_one.java
@@ -0,0 +1,35 @@
+/**
+ * Бані, компмех, завдання 1.1
+ * @author oleksandrbani
+ * @version 1.0
+ */
+
+package helloworld;
+
+public class first_one {
+     public static void main(String args[]){
+    	 
+    	 
+    	 int[] a = new int [5];
+    	 for (int i=0; i < a.length; i++)
+    	 {
+    		 System.out.println(a[i]);
+    	 }
+    	 
+    	 
+    	 char[] b = new char [5];
+    	 for (int i=0; i < b.length; i++)
+    	 {
+    		 System.out.print(b[i]); 
+    	 }
+    	 
+    	 
+    	 String[] c = new String [5];
+    	 for (int i=0; i < c.length; i++)
+    	 {
+    		 System.out.println(c[i]);
+    	 }
+    	 
+
+}
+}
